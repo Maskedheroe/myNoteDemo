741e8f1aa6b7454f880b37fcdd721ce0b8aeb6c8
Fang <296994719@qq.com>
2018-03-21 21:28:21 +0800

    3.21 Fang Commit

---
+++ app/src/main/java/com/example/asus/mynotebook/view/pages/minepager/GlideImageLoader.java
@@ -0,0 +1,19 @@
+package com.example.asus.mynotebook.view.pages.minepager;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+/**
+ * Created by asus on 2018/3/21.
+ */
+
+public class GlideImageLoader {
+    public static void glideLoaderByByte(Context context, Byte[] bytes, ImageView view){
+        Glide.with(context).load(bytes).into(view);
+    }
+    public static void glideLoaderByURL(Context context, String URL, ImageView view){
+        Glide.with(context).load(URL).into(view);
+    }
+}
